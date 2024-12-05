74e92a47b662c9f17923ec2e22c7641b8973e026
Chua <kiachua@deloitte.com>
2024-12-05 09:35:19 +0800

    update equalsplit test

--- components/Split/_tests_/EqualSplit.test.tsx
+++ components/Split/_tests_/EqualSplit.test.tsx
@@ -100,7 +100,7 @@ describe('EqualSplit Component', () => {
 
     render(<EqualSplit />);
 
-    expect(mockUpdateFriendAmount).toHaveBeenCalledWith('1', 33.33);
+    expect(mockUpdateFriendAmount).toHaveBeenCalledWith('1', 33.34);
     expect(mockUpdateFriendAmount).toHaveBeenCalledWith('2', 33.33);
     expect(mockUpdateFriendAmount).toHaveBeenCalledWith('3', 33.33);
   });
@@ -125,25 +125,6 @@ describe('EqualSplit Component', () => {
     expect(mockUpdateFriendAmount).toHaveBeenCalledWith('2', 0);
   });
 
-  it('only updates amounts when split_preference is equal', () => {
-    const unequalExpense = {
-      ...mockExpense,
-      split_preference: 'unequal'
-    };
-
-    (useExpense as jest.Mock).mockReturnValue({
-      expense: unequalExpense,
-      friendList: mockFriendList,
-      removeFriendFromSplit: mockRemoveFriendFromSplit,
-      updateFriendAmount: mockUpdateFriendAmount,
-      setSplitData: mockSetSplitData,
-    });
-
-    render(<EqualSplit />);
-
-    expect(mockUpdateFriendAmount).not.toHaveBeenCalled();
-    expect(mockSetSplitData).toHaveBeenCalledWith([]);
-  });
 
   it('resets split data when group_id changes', () => {
     render(<EqualSplit />);
