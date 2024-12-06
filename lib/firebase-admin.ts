1c1fb8fd9059fcc03b5bfd4e7e27df42ad99adb7
Chu <keachu@deloitte.com>
2024-12-06 23:20:24 +0800

    Fix: pwa issue

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,17 +1,19 @@
 import admin from 'firebase-admin';
 
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+};
+
 if (!admin.apps.length) {
-  if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PRIVATE_KEY) {
-    throw new Error('Missing Firebase Admin credentials');
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount)
+    });
+  } catch (error) {
+    console.error('Firebase admin initialization error:', error);
   }
-  
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    })
-  });
 }
 
 export default admin;
